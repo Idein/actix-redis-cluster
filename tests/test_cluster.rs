4b953fa6c72f6d387beac071af638c370744d62b
Kazuhiro Kobayashi <orihuzakihsayabok@gmail.com>
2019-02-26 18:26:13 +0900

    Tests for EvalSha

--- tests/test_cluster.rs
+++ tests/test_cluster.rs
@@ -116,3 +116,43 @@ fn test_cluster_load_script() {
 
     sys.run();
 }
+
+#[test]
+fn test_cluster_eval_sha() {
+    env_logger::try_init();
+    let sys = System::new("test");
+
+    let addr = RedisClusterActor::start(vec![
+        "127.0.0.1:7000".into(),
+        "127.0.0.1:7001".into(),
+        "127.0.0.1:7002".into(),
+    ]);
+
+    Arbiter::spawn_fn(move || {
+        addr.send(ScriptLoad {
+            script: r#"
+                redis.call('SET', KEYS[1], ARGV[1])
+                return tonumber(redis.call('GET', KEYS[2]))
+                "#,
+            slot: 15206, //slot of "actix"
+        })
+        .then(move |res| match res {
+            Ok(Ok(hash)) => addr
+                .send(EvalSha {
+                    hash: hash,
+                    keys: vec!["actix".into(), "actix".into()],
+                    args: vec!["21".into()],
+                })
+                .then(move |res| match &res {
+                    Ok(Ok(RespValue::Integer(21))) => {
+                        System::current().stop();
+                        Ok(())
+                    }
+                    _ => panic!("Should not happen {:?}", res),
+                }),
+            _ => panic!("Should not happen {:?}", res),
+        })
+    });
+
+    sys.run();
+}
